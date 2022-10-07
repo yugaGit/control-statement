c322b46fb68a5f9bf8fd66a6e5fd178ffc12dce0
Yugaraj.B <114376104+yugaGit@users.noreply.github.com>
2022-10-07 14:56:20 +0530

    Add files via upload
    program using (if else) control statement.

---
+++ Control.java
@@ -0,0 +1,20 @@
+public class Control{
+    
+    public static void main(String[]args){
+     
+    int x=55;
+    int y=-25;
+if (x>y){
+  
+   System.out.println("x is greater");
+
+  } else{
+
+   System.out.println("x is lesser");
+
+
+}
+
+}
+
+}
